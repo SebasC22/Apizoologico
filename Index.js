dc38ed32a456d9a365fded2f53f1f201b94e4e95
Sebastian <anonim@gmail.com>
2026-03-11 13:59:52 -0500

    Se agregan varias variables

--- Index.js
+++ Index.js
@@ -1,7 +1,10 @@
-'use strict';
- http = require('http');
-const server = http.createServer(function(req, res){
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end('Hola mundo');
-});
-server.listen(5000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/prueba', (req, res) =>{
+    res.send (' La vida es como un banano si no te comes el tuyo se lo quitas a tu mejor amiga. ')
+})
+app.listen(port, ()=>{
+    console.log('La aplicacion se esta ejecutando por el puerto ' + port)
+} )
