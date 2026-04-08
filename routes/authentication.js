d04027d4be18b91e17ce1bbec7373ddd1efbd206
asd <asd@asd.com>
2026-04-08 14:12:16 -0500

    Se agrego pal de cositas mas importantes

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const userSchema = require("../models/user");
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
     const { usuario, correo, clave } = req.body;
     const user = new userSchema({
         usuario: usuario,
         correo: correo,
-        clave: clave
+        clave: clave,
     });
-   
+
     router.post("/login", async (req, res) => {
-        
+
         const { error } = userSchema.validate(req.body.correo, req.body.clave);
         if (error) return res.status(400).json({ error: error.details[0].message });
-        
+
         const user = await userSchema.findOne({ correo: req.body.correo });
-      
+
         if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
-        
-      
+
+
         const validPassword = await bcrypt.compare(req.body.clave, user.clave);
         if (!validPassword)
             return res.status(400).json({ error: "Clave no válida" });
@@ -30,8 +31,12 @@ router.post('/signup', async (req, res) => {
     });
     user.clave = await user.encryptClave(user.clave);
     await user.save(); 
-    //res.json(user);
-    res.json(user);
-    message: "Usuario guardado."
+    const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+        expiresIn: 60 * 60 * 24, //un día en segundos
+    });
+    res.json({
+        auth: true,
+        token,
+    });
 });
 module.exports = router;
