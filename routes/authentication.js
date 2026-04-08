2e4885b2038b231da315cf28236f356197b15c58
asd <asd@asd.com>
2026-04-08 14:04:19 -0500

    Se agrega inicio

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const router = express.Router(); 
+const router = express.Router();
 const userSchema = require("../models/user");
 router.post('/signup', async (req, res) => {
     const { usuario, correo, clave } = req.body;
@@ -9,8 +9,27 @@ router.post('/signup', async (req, res) => {
         correo: correo,
         clave: clave
     });
+   
+    router.post("/login", async (req, res) => {
+        
+        const { error } = userSchema.validate(req.body.correo, req.body.clave);
+        if (error) return res.status(400).json({ error: error.details[0].message });
+        
+        const user = await userSchema.findOne({ correo: req.body.correo });
+      
+        if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
+        
+      
+        const validPassword = await bcrypt.compare(req.body.clave, user.clave);
+        if (!validPassword)
+            return res.status(400).json({ error: "Clave no válida" });
+        res.json({
+            error: null,
+            data: "Bienvenido(a)",
+        });
+    });
     user.clave = await user.encryptClave(user.clave);
-    await user.save(); //save es un método de mongoose para guardar datos en MongoDB 
+    await user.save(); 
     //res.json(user);
     res.json(user);
     message: "Usuario guardado."
